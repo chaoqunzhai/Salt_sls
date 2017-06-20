b56aa9ac2c33fcf87933634869f2f32626b439b5
cq <1047738527@qq.com>
2017-06-20 15:32:05 +0800

    20170620_1531

--- salt/base/_returners/local_redis.py
+++ salt/base/_returners/local_redis.py
@@ -3,5 +3,7 @@ import json
 
 
 def returner(ret):
-  pool = redis.ConnectionPool(host='172.16.30.133',port=6379)
+  pool = redis.ConnectionPool(host='172.16.30.133',port=6379,db='1',password='salt6379')
   redis_recv=redis.Redis(connection_pool=pool)
+  data= ret['jid'] + '__' + json.dumps(ret['return'])
+  redis_recv.lpush(__grains__['id'],data)
